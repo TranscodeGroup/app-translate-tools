94ddce080cdbeff3b157939967d96b8112baa7be
ipcjs.mac <gipcjs@gmail.com>
2022-11-09 19:10:03 +0800

    调整

--- tg_mt.py
+++ tg_mt.py
@@ -7,15 +7,15 @@ def open_files():
     return (
         ArbFile('../@tg/tg_flutter/maintain/lib/resources/l10n/app_zh.arb', 'zh'),
         ArbFile('../@tg/tg_flutter/maintain/lib/resources/l10n/app_en.arb', 'en'),
-        ArbFile('../@tg/tg_flutter/maintain/lib/resources/l10n/app_th.arb', 'th'),
+        # ArbFile('../@tg/tg_flutter/maintain/lib/resources/l10n/app_th.arb', 'th'),
     )
 
 
 def main():
-    translate_files(open_files(), translate_all_lang=True)
+    # translate_files(open_files(), translate_all_lang=True)
     # import_xls('./tmp/urikar-2021-04-14-result.xls', open_files(), delete_no_exist_item=False)
-    # export_xls('./tmp/tg-mt-2022-01-22.xls', open_files())
-    # import_xls('./tmp/tg-bmt-2022-01-22.xls', open_files())
+    export_xls('./tmp/tg-mt-2022-11-09.xls', open_files())
+    import_xls('./tmp/tg-mt-2022-11-09.xls', open_files())
     pass
 
 
